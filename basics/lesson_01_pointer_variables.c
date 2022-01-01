77316463efa787f1ccf457369e655605b5e1d584
pedro-git-projects <pedro.coding.contact@gmail.com>
2022-01-01 19:58:17 -0300

    indirection

--- basics/lesson_01_pointer_variables.c
+++ basics/lesson_01_pointer_variables.c
@@ -4,17 +4,17 @@
 int main(void) {
 
 	int num;
-	int *numPtr;
+	int *numPtr; // making so that this is a pointer variable
 	int num2;
 	
 	num = 100;
 	numPtr = &num; // storing the address of the variable num in the numPtr variable
 
-	num2 = *numPtr; // pointer to an int 
+	num2 = *numPtr; // using the indirection or dereferencing operator to retreive the value sotred at the pointer adress 
 
 	printf("num=%d, numPtr=%d, the adress of num=%d, and num2=%d\n", num, numPtr, &num, num2);
 
-	// adequate way to print a pointer
+	// adequate way to print a pointers and references
 	printf("integer: %d, pointer: %p, adress: %p, integer that was assigned with pointer: %d\n", num, numPtr, &num, num2);
 
 	return EXIT_SUCCESS;
